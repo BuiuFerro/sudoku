f2d6f1d60cf59ba9264eb30dcab526f19faaf769
buiuferro <bruno.ferro@fatec.sp.gov.br>
2020-11-03 21:05:20 -0300

    First commit

---
+++ app/src/business/positions.js
@@ -0,0 +1,15 @@
+let Position = {
+  TOPLEFT: [0, 0],
+  TOP: [0, 1],
+  TOPRIGHT: [0, 2],
+
+  MIDLEFT: [1, 0],
+  MIDDLE: [1, 1],
+  MIDRIGHT: [1, 2],
+
+  BOTLEFT: [2, 0],
+  BOTTOM: [2, 1],
+  BOTRIGHT: [2, 2],
+};
+
+module.exports = { Position };
