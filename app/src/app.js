e05c4936e2a88a04b07a42b229d13f44a88131cb
buiuferro <bruno.ferro@fatec.sp.gov.br>
2020-11-05 17:11:19 -0300

    Testing Pug

--- app/src/app.js
+++ app/src/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Sudoku!");
+app.set("view engine", "pug");
+
+app.get("/", async (req, res) => {
+  res.render("../app/src/views/index");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Sudoku game running at http://localhost:${port}`);
 });
