6b7c30472cbcdb6da04226f276494855a527a118
buiuferro <bruno.ferro@fatec.sp.gov.br>
2020-11-17 19:26:09 -0300

    Evolving to css

--- app/src/app.js
+++ app/src/app.js
@@ -1,13 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set("view engine", "pug");
-
-app.get("/", async (req, res) => {
-  res.render("../app/src/views/index");
-});
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(port, () => {
-  console.log(`Sudoku game running at http://localhost:${port}`);
-});
+  console.log(`App listening at http://localhost:${port}`)
+})
